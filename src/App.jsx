7e3a4f3a6fa4afce8df51d6d27d5334d5b1839b4
haroon-90 <haroonboy90@gmail.com>
2025-04-16 17:38:47 +0500

    link ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/navbar'
 import Main from './components/main'
 import Footer from './components/footer'
@@ -7,9 +8,11 @@ function App() {
 
   return (
     <>
-      <Navbar className="navbar-class " />
-      <Main className="flex flex-grow-1 min-h-[calc(100vh-128px)]" />
-      <Footer className="w-full bg-gray-800 text-white text-center p-4" />
+      <BrowserRouter basename="/BoloToh">
+        <Navbar className="navbar-class " />
+        <Main className="flex flex-grow-1 min-h-[calc(100vh-128px)]" />
+        <Footer className="w-full bg-gray-800 text-white text-center p-4" />
+      </BrowserRouter>
     </>
   )
 }
